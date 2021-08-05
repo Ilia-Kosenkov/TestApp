b1aa8bd0abb4ac8d120552c42aca384272dca362
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-05 21:43:07 +0300

    Trying out simple parsing

---
+++ TestApp.Test/DevTests.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApp.Test
+{
+    [TestClass]
+    public class DevTests
+    {
+        [DataTestMethod]
+        [DataRow("*", new int[] {})]
+        [DataRow("12", new[] { 12 })]
+        // [DataRow("1,2,3-5,10-20/3",  new [] {1,2,3,4,5,10,13,16,19})]
+        public void TestParsing(string strRep, int[] parsedValue)
+        {
+            var parser = new InputParser();
+
+            parser.Parse(strRep);
+            Assert.Fail();
+        }
+    }
+}
---
+++ TestApp/InputParser.cs
@@ -0,0 +1,42 @@
+﻿#nullable enable
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("TestApp.Test")]
+namespace TestApp
+{
+    internal class InputParser
+    {
+        public void Parse(ReadOnlySpan<char> input)
+        {
+            var prev = 0;
+            var curr = 0;
+            Input? result = null;
+            while (curr < input.Length)
+            {
+                var ch = input[curr++];
+                if (char.IsDigit(ch))
+                {
+                    continue;
+                }
+
+                if (ch == ',')
+                {
+                    // Parse(input[prev..curr]);
+                    prev = curr;
+                }
+            }
+
+            if (input[prev..curr].Trim().Equals("*", StringComparison.OrdinalIgnoreCase))
+            {
+                result = new AnyInput();
+            }
+
+            if (int.TryParse(input[prev..curr], out var intVal))
+            {
+                result = new SingularInput() { Value = intVal };
+            }
+
+        }
+    }
+}
