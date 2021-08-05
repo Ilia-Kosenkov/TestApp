0235e9dc1b4ed27e78827bb285a7c7b906251207
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-05 21:08:12 +0300

    Outlined basic interaces

---
+++ TestApp/ISchedule.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+using System;
+
+namespace TestApp
+{
+    public interface ISchedule
+    {
+        /// <summary>
+        /// Возвращает следующий ближайший к заданному времени момент в расписании или
+        /// само заданное время, если оно есть в расписании.
+        /// </summary>
+        /// <param name="t1">Заданное время</param>
+        /// <returns>Ближайший момент времени в расписании</returns>
+        DateTime NearestEvent(DateTime t1);
+
+        
+        /// <summary>
+        /// Возвращает предыдущий ближайший к заданному времени момент в расписании или
+        /// само заданное время, если оно есть в расписании.
+        /// </summary>
+        /// <param name="t1">Заданное время</param>
+        /// <returns>Ближайший момент времени в расписании</returns>
+        DateTime NearestPrevEvent(DateTime t1);
+
+        /// <summary>
+        /// Возвращает следующий момент времени в расписании.
+        /// </summary>
+        /// <param name="t1">Время, от которого нужно отступить</param>
+        /// <returns>Следующий момент времени в расписании</returns>
+        DateTime NextEvent(DateTime t1);
+
+        /// <summary>
+        /// Возвращает предыдущий момент времени в расписании.
+        /// </summary>
+        /// <param name="t1">Время, от которого нужно отступить</param>
+        /// <returns>Предыдущий момент времени в расписании</returns>
+        DateTime PrevEvent(DateTime t1);
+    }
+}
