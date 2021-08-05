8f99f25afd2d66837ae4fac941e731acaa2fdb14
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-06 00:57:45 +0300

    Verifying input parsing

--- TestApp.Test/DevTests.cs
+++ TestApp.Test/DevTests.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestApp.Test
@@ -7,15 +8,22 @@ namespace TestApp.Test
     public class DevTests
     {
         [DataTestMethod]
-        [DataRow("*", new int[] {})]
-        [DataRow("12", new[] { 12 })]
-        [DataRow("3-7", new[] { 3, 4, 5, 6, 7 })]
-        [DataRow("1,2,3-5,10-20/3",  new [] {1,2,3,4,5,10,13,16,19})]
-        public void TestParsing(string strRep, int[] parsedValue)
+        [DataRow("*", new uint[] {0, 1, 2}, 3)]
+        [DataRow("12", new uint[] { 12 }, 3)]
+        [DataRow("3-7", new uint[] { 3, 4, 5, 6, 7 }, 3)]
+        [DataRow("*/4", new uint[] {0, 4, 8, 12, 16, 20}, 24)]
+        [DataRow("1,2,3-5,10-20/3",  new uint[] {1,2,3,4,5,10,13,16,19}, 32)]
+        public void TestParsing(string strRep, uint[] parsedValue, int limit)
         {
             var parser = new InputParser();
 
+            var buff = new uint [512];
+            
             var result = parser.Parse(strRep);
+
+            Assert.IsTrue(result.TryWriteValues(buff, (uint)limit, out var nWritten));
+            Assert.AreEqual(parsedValue.Length, (int)nWritten);
+            Assert.IsTrue(parsedValue.AsSpan().SequenceEqual(buff.AsSpan(0, (int)nWritten)));
         }
     }
 }
--- TestApp/Input.cs
+++ TestApp/Input.cs
@@ -7,23 +7,91 @@ namespace TestApp
 {
     internal abstract record Input
     {
-
+        public abstract bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten);
     }
 
     internal record AnyInput : Input
     {
+        public override bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten)
+        {
+            if (buffer.Length >= upperLimit)
+            {
+                for (var i = 0u; i < upperLimit; i++)
+                {
+                    buffer[(int)i] = i;
+                }
+
+                valsWritten = upperLimit;
+                return true;
+            }
+
+            valsWritten = 0;
+            return false;
+        }
     }
 
     internal record SingularInput(uint Value) : Input
     {
+        public override bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten)
+        {
+            if (buffer.Length > 1)
+            {
+                buffer[0] = Value;
+                valsWritten = 1;
+                return true;
+            }
+            valsWritten = 0;
+            return false;
+        }
     }
 
     internal record RangeInput(uint LowerLimit, uint UpperLimit) : Input
     {
+        public override bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten)
+        {
+            if (buffer.Length >= UpperLimit - LowerLimit)
+            {
+                for(var i = LowerLimit; i <= UpperLimit; i++)
+                {
+                    buffer[(int)(i - LowerLimit)] = i;
+                }
+
+                valsWritten = UpperLimit - LowerLimit + 1u;
+                return true;
+            }
+
+            valsWritten = 0;
+            return false;
+        }
     }
 
     internal record StepByInput(Input ValueRange, uint StepBy) : Input
     {
+        public override bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten)
+        {
+            valsWritten = 0;
+            var (from, to) = ValueRange switch
+            {
+                AnyInput => (0u, upperLimit),
+                RangeInput {LowerLimit : var lhs, UpperLimit : var rhs} => (lhs, rhs + 1),
+                _ => (0u, 0u)
+            };
+
+            if (buffer.Length >= (to - from - 1) / StepBy)
+            {
+                var val = from;
+                var i = 0;
+                for(; val < to; val += StepBy, i++)
+                {
+                    buffer[i] = val;
+                }
+
+                valsWritten = (uint)i;
+                return true;
+            }
+
+            return false;
+        }
     }
 
     internal record ListInput : Input
@@ -50,6 +118,23 @@ namespace TestApp
         {
             _elements.Add(i);
             return this;
+        }
+
+        public override bool TryWriteValues(Span<uint> buffer, uint upperLimit, out uint valsWritten)
+        {
+            valsWritten = 0;
+            foreach (var item in _elements)
+            {
+                if (!item.TryWriteValues(buffer, upperLimit, out var nVals))
+                {
+                    return false;
+                }
+
+                buffer = buffer.Slice((int)nVals);
+                valsWritten += nVals;
+            }
+
+            return true;
         }
     }
 }
