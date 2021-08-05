06f7de98b3e590ca03195262f3174a525558d651
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-05 21:10:21 +0300

    Outlined simple schedule, first attempt at problem

---
+++ TestApp/SimpleSchedule.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System;
+
+namespace TestApp
+{
+    public class SimpleSchedule : ISchedule
+    {
+        public DateTime NearestEvent(DateTime t1) => throw new NotImplementedException();
+
+        public DateTime NearestPrevEvent(DateTime t1) => throw new NotImplementedException();
+
+        public DateTime NextEvent(DateTime t1) => throw new NotImplementedException();
+
+        public DateTime PrevEvent(DateTime t1) => throw new NotImplementedException();
+    }
+}
---
+++ TestApp/SimpleScheduleProvider.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+
+namespace TestApp
+{
+    public class SimpleScheduleProvider : IScheduleProvider
+    {
+        public ISchedule GetSchedule() => throw new System.NotImplementedException();
+
+        public ISchedule GetSchedule(string scheduleString) => throw new System.NotImplementedException();
+    }
+}
