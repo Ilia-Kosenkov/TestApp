d37ae08eaf9e4b361d0ac7185f37fb570a5eb63c
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-05 21:20:18 +0300

    Created Input to represent date time values

---
+++ TestApp/Input.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#nullable enable
+namespace TestApp
+{
+    internal abstract record Input
+    {
+        
+    }
+
+    internal record AnyInput : Input
+    {
+    }
+
+    internal record SingularInput : Input
+    {
+        public int Value { get; init; }
+    }
+
+    internal record RangeInput : Input
+    {
+        public int LowerLimit { get; init; }
+        public int UpperLimit { get; init; }
+    }
+
+    internal record StepByInput : Input
+    {
+        public Input ValueRange { get; init; } = new AnyInput();
+        public int StepBy { get; init; }
+    }
+
+    internal record ListInput : Input
+    {
+        private readonly Input[] _elements = Array.Empty<Input>();
+
+        public ListInput(params Input[] input)
+        {
+            if (_elements.Length != input.Length)
+            {
+                _elements = new Input[input.Length];
+            }
+
+            input.CopyTo(_elements.AsSpan());
+        }
+
+        public ListInput(IEnumerable<Input> input) : this(input.ToArray())
+        {
+        }
+    }
+}
