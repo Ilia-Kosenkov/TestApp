bb58f01c7003e05d11cea91061edf6af479da3e3
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-08-06 15:01:21 +0300

    Container for parsed entry

---
+++ TestApp/ScheduleRep.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+namespace TestApp
+{
+    internal record ScheduleRep()
+    {
+        public Input Years { get; init; } = AnyInput.Any;
+        public Input Months { get; init; } = AnyInput.Any;
+        public Input Days { get; init; } = AnyInput.Any;
+        
+        public Input WeekDays { get; init; } = AnyInput.Any;
+        
+        public Input Hours { get; init; } = AnyInput.Any;
+        public Input Minutes { get; init; } = AnyInput.Any;
+        public Input Seconds { get; init; } = AnyInput.Any;
+
+        public Input Milliseconds { get; init; } = AnyInput.Any;
+    }
+}
